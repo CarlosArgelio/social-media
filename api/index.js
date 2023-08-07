507765f70191154bff65f9f44b992281f41605be
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-08-06 23:43:22 -0400

    feat(swagger): documentation project

--- api/index.js
+++ api/index.js
@@ -4,15 +4,15 @@ const bodyParser = require('body-parser')
 const config = require('../config')
 const users = require('./components/users/network')
 // swagger
-// const swaggerUi = require('swagger-ui-express')
-// const swaggerJsDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('../swagger/swagger.json')
 
 const app = express()
 
 app.use(bodyParser.json())
 
 // middlewares
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJsDoc(config.swagger.swaggerOptions)))
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 // ROUTERS
 app.use('/api/users', users)

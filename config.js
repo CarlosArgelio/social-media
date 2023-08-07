3216aa572bfaf6ae341f7998570b972e0b51537b
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-08-06 22:24:00 -0400

    lab: documentation swagger rm

--- config.js
+++ config.js
@@ -1,28 +1,7 @@
 require('dotenv').config()
-const path = require('path')
 
 module.exports = {
     api: {
       port: process.env.API_PORT || 3000
-    },
-    swagger: {
-      swaggerOptions: {
-        definition: {
-          openapi: '3.0.0',
-          info: {
-            title: 'API - Social Media example',
-            version: '1.0.0',
-          },
-          servers: [
-            {
-              url: process.env.URL_DOCS || 'http://localhost:3000',
-
-            }
-          ]
-      },
-      apis: [
-        `${path.join(__dirname, './api/components/users/network.js')}`
-      ]
     }
-  }
 }

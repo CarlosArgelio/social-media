63e2a1d004d9431581b61c657face47d9f2d9585
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-08-07 23:02:16 -0400

    feat(components/users): add logic upsert user to auth

--- api/components/users/controller.js
+++ api/components/users/controller.js
@@ -1,6 +1,7 @@
 // const nanoid = require('nanoid');
 const uuid = require('uuid');
 const boom = require('@hapi/boom');
+const auth = require('../auth');
 
 const TABLE = 'users';
 
@@ -22,15 +23,25 @@ module.exports = (injectedStore) => {
       return user
     }
 
-    function upsert(body) {
+    async function upsert(body) {
         const user = {
             name: body.name,
+            username: body.username,
+            password: body.password,
         };
 
         if (body.id) {
             user.id = body.id;
         } else {
             user.id = uuid.v4();
+        }
+
+        if (body.password || body.username) {
+            await auth.upsert({
+              id: user.id,
+              username: user.username,
+              password: user.password,
+            })
         }
 
         return store.upsert(TABLE, user);

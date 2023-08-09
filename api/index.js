754bb5bb723791d896166a6a326073c51e543161
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-08-08 21:35:15 -0400

    feat: errors validator

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ const auth = require('./components/auth/network');
 
 // middlewares
 const { logErrors, boomErrorHandler, errorHandler } = require('../middlewares/error.handler');
+// const errors = require('../network/errors')
 
 // swagger
 const swaggerUi = require('swagger-ui-express');
@@ -25,6 +26,8 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(logErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
+
+// app.use(errors)
 
 
 app.listen(config.api.port, () => {
